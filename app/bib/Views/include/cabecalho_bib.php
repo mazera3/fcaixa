10441dc5e85325794f2a6b160913dca8c957946f
Edio.Mazera <mazera3@gmail.com>
2021-05-17 21:35:42 -0300

    JavaScript form

--- app/bib/Views/include/cabecalho_bib.php
+++ app/bib/Views/include/cabecalho_bib.php
@@ -17,6 +17,7 @@ if (!defined('URL')) {
     <script type="text/javascript" src="https://unpkg.com/webcam-easy/dist/webcam-easy.min.js"></script>
         <script defer src="<?php echo URLADM . 'assets/js/fontawesome-all.min.js'; ?>"></script>
         <script defer src="<?php echo URLADM . 'app/bib/assets/js/webcam-easy.min.js'; ?>"></script>
+        <script defer src="<?php echo URLADM . 'app/bib/assets/js/form.js'; ?>"></script>
         <script defer src="<?php echo URLADM . 'assets/fontawesome/js/fontawesome.min.js'; ?>"></script>
         <link rel="stylesheet" href="<?php echo URLADM . 'assets/css/fontawesome.min.css'; ?>">
         <link rel="stylesheet" href="<?php echo URLADM . 'assets/fontawesome/css/fontawesome.min.css'; ?>">
